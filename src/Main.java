4d71fbc08ec0a595153ecba0c7a25f80ed638c0f
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-11-04 08:49:12 -0500

    why no commit these

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int dayMax;
         int year = InputHelper.getRangedInt(scan, "Enter your birth Year", 1950,2010);
         int month = InputHelper.getRangedInt(scan, "Enter your birth Month", 1,12);
-        int day = InputHelper.getRangedInt(scan, "Enter your birth day", 1,31);
+        if(month == 1 && month == 3 && month == 5 && month == 7 && month == 8 && month == 10 && month == 12){
+            dayMax =31;
+        } else if (month == 2){
+            dayMax = 29;
+        } else {
+            dayMax = 30;
+        }
+        int day = InputHelper.getRangedInt(scan, "Enter your birth day", 1,dayMax);
         int hour = InputHelper.getRangedInt(scan, "Enter your birth hour", 1,24);
         int minute = InputHelper.getRangedInt(scan, "Enter your birth minute",1,59);
 
