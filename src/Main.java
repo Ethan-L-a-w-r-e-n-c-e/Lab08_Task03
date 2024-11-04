aac9720933f9d7d2796b31729aca3260e0833b3f
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-11-04 08:36:20 -0500

    why no commit these

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Scanner scan = new Scanner(System.in);
+        int year = InputHelper.getRangedInt(scan, "Enter your birth Year", 1950,2010);
+        int month = InputHelper.getRangedInt(scan, "Enter your birth Month", 1,12);
+        int day = InputHelper.getRangedInt(scan, "Enter your birth day", 1,31);
+        int hour = InputHelper.getRangedInt(scan, "Enter your birth hour", 1,24);
+        int minute = InputHelper.getRangedInt(scan, "Enter your birth minute",1,59);
+
+        System.out.println("Year: " + year);
+        System.out.println("Month: " + month);
+        System.out.println("Day: " + day);
+        System.out.println("Hour: " + hour);
+        System.out.println("Minute: " + minute);
+}
 }
